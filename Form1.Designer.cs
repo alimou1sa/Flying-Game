f1c7e09cc62afd96d4c13a267b55d791f12ac56b
Dev.Ali Mousa <ali.mou1sa@gmail.com>
2024-10-05 12:04:26 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Project_7_Flying_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.plyer1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.lbl_score = new System.Windows.Forms.Label();
+            this.lb_over = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plyer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Project_7_Flying_game.Properties.Resources.download1;
+            this.pictureBox1.Location = new System.Drawing.Point(2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(313, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "pilar";
+            // 
+            // plyer1
+            // 
+            this.plyer1.BackColor = System.Drawing.Color.Transparent;
+            this.plyer1.Image = global::Project_7_Flying_game.Properties.Resources.download;
+            this.plyer1.Location = new System.Drawing.Point(144, 135);
+            this.plyer1.Name = "plyer1";
+            this.plyer1.Size = new System.Drawing.Size(87, 52);
+            this.plyer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.plyer1.TabIndex = 0;
+            this.plyer1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Project_7_Flying_game.Properties.Resources.download1;
+            this.pictureBox2.Location = new System.Drawing.Point(432, -1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(299, 72);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "pilar";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Project_7_Flying_game.Properties.Resources.download1;
+            this.pictureBox3.Location = new System.Drawing.Point(1, 330);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(314, 72);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "pilar";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Project_7_Flying_game.Properties.Resources.download1;
+            this.pictureBox4.Location = new System.Drawing.Point(432, 330);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(298, 72);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "pilar";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox5.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(732, 35);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Project_7_Flying_game.Properties.Resources.images;
+            this.pictureBox6.Location = new System.Drawing.Point(589, 77);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(60, 43);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "enemy";
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::Project_7_Flying_game.Properties.Resources.images;
+            this.pictureBox7.Location = new System.Drawing.Point(345, 262);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(56, 49);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "enemy";
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::Project_7_Flying_game.Properties.Resources.images;
+            this.pictureBox8.Location = new System.Drawing.Point(12, 86);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(58, 43);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "enemy";
+            // 
+            // lbl_score
+            // 
+            this.lbl_score.AutoSize = true;
+            this.lbl_score.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lbl_score.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_score.ForeColor = System.Drawing.Color.White;
+            this.lbl_score.Location = new System.Drawing.Point(299, 5);
+            this.lbl_score.Name = "lbl_score";
+            this.lbl_score.Size = new System.Drawing.Size(125, 28);
+            this.lbl_score.TabIndex = 9;
+            this.lbl_score.Text = "Score : 0";
+            // 
+            // lb_over
+            // 
+            this.lb_over.AutoSize = true;
+            this.lb_over.BackColor = System.Drawing.Color.Transparent;
+            this.lb_over.Font = new System.Drawing.Font("Times New Roman", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_over.ForeColor = System.Drawing.SystemColors.Control;
+            this.lb_over.Location = new System.Drawing.Point(237, 178);
+            this.lb_over.Name = "lb_over";
+            this.lb_over.Size = new System.Drawing.Size(286, 67);
+            this.lb_over.TabIndex = 11;
+            this.lb_over.Text = "game over";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Project_7_Flying_game.Properties.Resources.Screenshot_2024_06_12_184752;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(732, 403);
+            this.Controls.Add(this.lb_over);
+            this.Controls.Add(this.lbl_score);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.plyer1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plyer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox plyer1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label lbl_score;
+        private System.Windows.Forms.Label lb_over;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_7_Flying_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lb_over.Visible = false;
+        }
+
+        bool up, down;
+        int Score;
+
+        void game_result()
+        {
+            foreach(Control x in this .Controls)
+            {
+                if(x is PictureBox && x.Tag=="pilar")
+                {
+                    foreach(Control j in this .Controls)
+                    {
+                        if(j is PictureBox && j.Tag=="enemy")
+                        {
+                            if (plyer1.Bounds.IntersectsWith(x.Bounds) ||
+                                (plyer1.Bounds.IntersectsWith(j.Bounds)))
+                            {
+                                lb_over.Visible = true;
+                                timer1.Stop();
+                            }
+
+
+                        }
+                    }
+                }
+            }
+        }
+
+        void Smoke()
+        {
+            PictureBox smoke = new PictureBox();
+            smoke.BackColor = System.Drawing.Color.Gold;
+            smoke.Width = 3;
+            smoke.Height = 3;
+            smoke.Top = plyer1.Top + plyer1.Height/2 ;
+            smoke.Left = plyer1.Left + plyer1.Width - 35;
+            smoke.Tag = "smoke";
+            this.Controls.Add(smoke);
+
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox&& x.Tag=="smoke")
+                {
+                    x.Left -= 10;
+                    if (x.Left < 0)
+                    {
+                        this.Controls.Remove(x);
+                        x.Dispose();
+
+                    }
+                }
+            }
+
+        }
+
+        void enemy_move()
+        {
+            Random rdn = new Random();
+            int x;
+            foreach(Control j in this.Controls)
+            {
+                if(j is PictureBox && j.Tag=="enemy")
+                {
+                    j.Left -=5;
+                    if (j.Left < 0)
+                    {
+
+                        x = rdn.Next(80, 300);
+                        j.Location = new Point(800, x);
+                        Score++;
+                        lbl_score.Text = "Score : " + Score;
+                    }
+                }
+            }
+
+
+        }
+
+        void Plyer_move()
+        {
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox &&  x.Tag=="pilar")
+                {
+                    x.Left-= 5;
+
+                    if (x.Left < -250)
+                    {
+                        x.Left = 700;
+                    }
+                }
+            }
+
+        }
+
+
+        void Pilar_move()
+        {
+            if(up==true)
+            {
+                plyer1.Top -= 5;
+            }
+            if(down==true)
+            {
+                plyer1.Top += 5;
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            switch (e.KeyData)
+            {
+                case Keys.Up:
+                    up = true;
+                    break;
+                case Keys.Down:
+                    down = true;
+                    break;
+
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Up:
+                    up = false;
+                    break;
+                case Keys.Down:
+                    down = false;
+                    break;
+
+            }
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Plyer_move();
+            Pilar_move();
+            enemy_move();
+            Smoke();
+            game_result();
+        }
+
+
+    }
+}
